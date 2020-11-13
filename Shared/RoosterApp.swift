31041d02d14e5ee18bd0ec78ba79832cefd928ae
Mike Fullerton <mfullerton@apple.com>
2020-11-13 08:34:53 -0800

    Initial Commit

---
+++ Shared/RoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoosterApp.swift
+//  Shared
+//
+//  Created by Mike Fullerton on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct RoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
