d2d36e17dee1d74ae9ef52f4f230a14c49c617d9
Mike Fullerton <mfullerton@apple.com>
2020-12-19 10:09:06 -0800

    wip

---
+++ RoosterAppKitPlugin/SparkleSupport.swift
@@ -0,0 +1,32 @@
+//
+//  SparkleSupport.swift
+//  RoosterAppKitPlugin
+//
+//  Created by Mike Fullerton on 12/19/20.
+//
+
+import Foundation
+import Sparkle
+
+extension AppKitPlugin {
+    public class SparkleController : NSObject {
+        
+        let updater: SUUpdater
+        
+        public init(withBundle appBundle: Bundle) {
+            self.updater = SUUpdater.init(for: appBundle)
+        }
+        
+        
+    }
+}
+/*
+ Public key (SUPublicEDKey value) for this key is:
+
+ DP+imgyOegheuDKQfK7ur191ScNFBpkTLFoTBJHfF30=
+ 
+ 
+ Public key (SUPublicEDKey value) for this key is:
+
+ DP+imgyOegheuDKQfK7ur191ScNFBpkTLFoTBJHfF30=
+ */
